a908bc639b9627eb72401b2000e7949bac10815b
Dang Nguyen <dang123vvk@gmail.com>
2023-09-17 15:21:34 +0700

    Update

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
-
+var cors = require('cors')
 const app = express();
-
+app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
@@ -14,3 +14,6 @@ app.use('/', routes);
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
 
 module.exports = app;
+// app.listen(3000, () => {
+//     console.log(`Example app listening on port 3000`)
+//   })
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,41 @@
 const { Router } = require('express');
+var { tall } = require('tall')
+
+async function someFunction(link) {
+    try {
+      const unshortenedUrl = await tall(link)
+      console.log('Tall url', unshortenedUrl)
+      return unshortenedUrl
+      
+    } catch (err) {
+      console.error('AAAW 👻', err)
+      return 'error'
+    }
+  }
 const { SuccessResponseObject } = require('../common/http');
 const demo = require('./demo.route');
 
+
 const r = Router();
 
-r.use('/demo', demo);
+// r.use('/demo', demo);
 
 r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
-
+r.post('/get-link', async (req, res) => {
+    console.log("link", req.body);
+    try {
+        const { link } = req.body;
+        console.log("link", link);
+        const response = await someFunction(link)
+        console.log("response", response);
+        if (response) {
+        //   const fullURL = response.headers.location;
+          res.json({ response });
+        } else {
+          res.json({ response: link}); // No redirect, so return the provided URL as is.
+        }
+      } catch (error) {
+        res.status(500).json({ error: 'Failed to expand the shortened URL' });
+      }
+});
 module.exports = r;
